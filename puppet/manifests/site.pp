492b95e872042a21b40ea621ef21f31838328076
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-05-15 14:04:04 +0100

    Remove vagrant stuff from site.pp

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,14 +1,5 @@
 node default {
-  file { '/home/vagrant/.hushlogin':
-    owner   => 'vagrant',
-    group   => 'vagrant',
-    content => '',
-  }
   exec { 'apt-get-update':
     command => '/usr/bin/apt-get update || true',
-  }
-  package { 'language-pack-en': 
-    ensure  => installed,
-    require => Exec['apt-get-update']
   }
 }
