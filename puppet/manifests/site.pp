2988121f53743de81b1c113d3e3c0b3a860cbff2
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-05-19 14:22:44 +0100

    Default deny on firewall but allow SSH

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,6 +1,14 @@
+Exec {
+  path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+}
+
 node default {
+  include ufw
   exec { 'apt-get-update':
     command => '/usr/bin/apt-get update || true',
   }
-  notify { 'Puppet is running on machine': }
+  ufw::allow { "allow-ssh-from-all":
+    port => 22,
+    ip   => 'any'
+  }
 }
