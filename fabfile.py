c7f40ab4780c445d34d7dcc804af725687445519
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-12-02 22:15:26 +0000

    Change the importing of bootstrap.py

--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python
-from fabric import state
-from fabric.api import (abort, env, get, hide, local, puts, run, runs_once,
-                            settings, sudo, task, warn)
-from fabric.task_utils import crawl
 
-import textwrap
+from bootstrap import *
 
-import bootstrap
-
-@task
-def help(name):
-    """Show extended help for a task (e.g. 'fab help:bootstrap.changepassword')"""
-    task = crawl(name, state.commands)
-
-    if task is None:
-        abort("%r is not a valid task name" % task)
-
-    puts(textwrap.dedent(task.__doc__).strip())
